63a6970d18c8c3b1dd52d58f57e21683dd49f52f
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-20 15:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 let newarr=[];
-let new-2-arr=[];
+let newarr2=[];
 
 const divide = (arr, n) => {
   // Write your code here
@@ -9,13 +9,13 @@ const divide = (arr, n) => {
 			sum=sum+arr[i];
 			if(sum>n)
 			{
-			newarr.push(new-2-arr);
-				new-2-arr=[];
+			newarr.push(new2arr);
+				new2arr=[];
 				
 			}
 		}
 	return newarr;
 };
 
-const n = prompt("Enter n: ");
+const n = prompt("Enter n:");
 alert(JSON.stringify(divide(arr, n)));
