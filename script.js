e610dcfbb030886757c318341bab68ab0633c883
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-20 15:08:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+let newarr=[];
+let new-2-arr=[];
 
 const divide = (arr, n) => {
   // Write your code here
+	for(let i=0;i<arr.length;i++)
+		{
+			sum=sum+arr[i];
+			if(sum>n)
+			{
+			newarr.push(new-2-arr);
+				new-2-arr=[];
+				
+			}
+		}
+	return newarr;
 };
 
 const n = prompt("Enter n: ");
